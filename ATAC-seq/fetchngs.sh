ad47a263a1d35f406bd10066db612bb1ed391974
uninchan <145501317+uninchan@users.noreply.github.com>
2023-10-30 13:23:16 -0400

    Create fetchngs.sh

---
+++ ATAC-seq/fetchngs.sh
@@ -0,0 +1,23 @@
+#! /bin/bash 
+#SBATCH --job-name=fetchngs_atacseq
+#SBATCH --output /OUTDIR/test-%j.out
+#SBATCH --error /OUTDIR/test-%j.err
+#SBATCH --partition=CLUSTER_NAME   # cluster-specific
+#SBATCH --nodes=1
+#SBATCH --ntasks=12
+#SBATCH --time=48:00:00   # HH/MM/SS
+#SBATCH --mem=120G   # memory requested, units available: K,M,G,T
+
+set -e
+
+spack load python@3.7.7%gcc@8.2.0 arch=linux-centos7-skylake_avx512
+spack load singularity
+spack load openjdk@11.0.12_7 arch=linux-centos7-skylake_avx512
+
+nextflow run nf-core/fetchngs -r 1.10.0 \
+--input /INDIR/GSM_Acc_List.csv \
+--outdir <OUTDIR> \
+-profile singularity \
+--nf_core_pipeline atacseq
+
+done
